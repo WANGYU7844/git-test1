6514b49e293434a16daf34ced048b6f3c172d9cf
wangyu <2690392092@qq.com>
2022-04-17 16:50:41 +0800

    first commit

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.wang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gitl");
+        System.out.println("hello git2l");
     }
 }
